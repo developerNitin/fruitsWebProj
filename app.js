9b1dc08c20104b40f2687ff99c13af03a93b75cd
Nitin Birdi <nitin.birdi@icloud.com>
2020-03-11 21:19:27 +0530

    mongoose started

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 // jshint esversion:6
+const mongoose = require('mongoose');
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+mongoose.connect("mongodb://localhost:27017/fruitsdb", {  useNewUrlParser: true});
 
-const url = 'mongodb://localhost:27017';
-
-const dbName = 'fruitsdb';
-
-const client = new MongoClient(url, {
-  useUnifiedTopology: true
-});
-
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  findDocuments(db, function() {
-    client.close();
-  });
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
 });
 
 const insertDocuments = function(db, callback) {
