c9e801ef0c2c34f0765b7f976c17076b65e49f54
Nitin Birdi <nitin.birdi@icloud.com>
2020-03-13 02:56:20 +0530

    finished

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // jshint esversion:6
 const mongoose = require('mongoose');
-
 mongoose.connect("mongodb://localhost:27017/fruitdb", {useUnifiedTopology: true,useNewUrlParser: true});
+
+
 
 const fruitSchema = new mongoose.Schema({
   name: {
-    type: Number,
-    required: [true, "please check you entry"]
+    type: String,
+    required: [true, "please check your entry"]
   },
   rating: {
     type: Number,
@@ -23,22 +24,6 @@ const fruit = new Fruit({
   rating: 5,
   review: "Pretty solid as a fruit"
 });
-
-fruit.save();
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  age: Number
-});
-
-const Person = mongoose.model("Person", personSchema);
-
-const person = new Person({
-  name: "John deo",
-  age: 19
-});
-
-person.save();
 
 const kiwi = new Fruit({
   name: "Kiwi",
@@ -58,88 +43,75 @@ const banana = new Fruit({
   review: "Pretty sweet"
 });
 
-Fruit.insertMany([kiwi, orange, banana], function(err){
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("successful");
-  }
+const pineApple = new Fruit({
+  name: "pineApple",
+  rating: 8,
+  review: "Pretty sweet"
 });
 
-Fruit.find(function(err, fruits) {
-  if (err) {
-    console.log(err);
-  } else {
-    //console.log(fruits);
-    mongoose.connection.close();
-    fruits.forEach(function(fruit) {
-      console.log(fruit.name);
-    });
-  }
+//pineApple.save();
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favouriteFruit: fruitSchema
 });
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: "John deo",
+  age: 19,
+  favouriteFruit: pineApple
+});
+
+//person.save();
+
+
+
+// Fruit.insertMany([kiwi, orange, banana], function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("successful");
+//   }
+// });
+
+// Fruit.find(function(err, fruits) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     //console.log(fruits);
+//     mongoose.connection.close();
+//     fruits.forEach(function(fruit) {
+//       console.log(fruit.name);
+//     });
+//   }
+// });
 
 // --------------> used to update a fruit
-Fruit.updateOne({_id: demoID}, {name: "Peach"}, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Success");
-  }
-});
+// Fruit.updateOne({_id: demoID}, {name: "Peach"}, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Success");
+//   }
+// });
 // --------------> used to delete a fruit
-Fruit.deleteOne(//{_id: } or
-  {name: "Peach"}, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Success");
-  }
-});
+// Fruit.deleteOne(//{_id: } or
+//   {name: "Peach"}, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Success");
+//   }
+// });
 // --------------> used to delete fruits
-Person.deleteMany({name : "John deo"}, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    mongoose.connection.close();
-    console.log("Success");
-  }
-});
-
-//
-// const insertDocuments = function(db, callback) {
-//   const collection = db.collection('fruits');
-//
-//   collection.insertMany([
-//     {
-//       name: "Apple",
-//       score: 8,
-//       review: "Great fruit"
-//     },
-//     {
-//       name: "Orange",
-//       score: 6,
-//       review: "Great sour"
-//     },
-//     {
-//       name: "Banana",
-//       score: 9,
-//       review: "Great stuff!"
-//     }
-// ], function(err, result) {
-//     assert.equal(err, null);
-//     assert.equal(3, result.result.n);
-//     assert.equal(3, result.ops.length);
-//     console.log("Inserted 3 documents into the collection");
-//     callback(result);
-//   });
-// };
-//
-// const findDocuments = function(db, callback) {
-//   const collection = db.collection('fruits');
-//   collection.find({}).toArray(function(err, fruits) {
-//     assert.equal(err, null);
-//     console.log("Found the following records");
-//     console.log(fruits);
-//     callback(fruits);
-//   });
-// };
+// Person.deleteMany({name : "John deo"}, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     mongoose.connection.close();
+//     console.log("Success");
+//   }
+// });
